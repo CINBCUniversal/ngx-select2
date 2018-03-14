d8d2b74770fb1b9bc7e6e1d3c06fea6b7c3304ed
Ronald Salmon <salmon@railroad19.com>
2018-03-14 12:11:28 -0600

    import reactive forms and remove validator

--- index.ts
+++ index.ts
@@ -1,20 +1,19 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import {LSelect2Component} from './select2/select2.component';
-import {LSelect2Validator} from "./select2/select2.validator";
 
 export * from './select2/select2.component';
 
 @NgModule({
   declarations: [
-    LSelect2Component,
-    LSelect2Validator
+    LSelect2Component
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     LSelect2Component
